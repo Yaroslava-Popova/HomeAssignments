7bc79b7f56d483cc187a4501cffa6d0dbac884ad
Yaroslava <st132757@student.spbu.ru>
2024-09-28 11:50:00 +0300

    Добавлен файл  hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+/*  Popova Yaroslava
+    st132757@student.spbu.ru
+    project №1 "hello world"
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+extern string hi;
+
+void hello() {
+    hi = "Hello, world!";
+    cout <<hi;
+}
