91aa3cf347f4d8955306862ad2721aa741bd09b4
Yaroslava <st132757@student.spbu.ru>
2024-12-19 19:10:38 +0300

    made a header file

---
+++ Assignment2a/header.h
@@ -0,0 +1,18 @@
+/* Popova Yaroslava
+    st132757@student.spbu.ru
+*/
+
+#ifndef REVERSE_h
+#define REVERSE_h
+
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+namespace problem_2a
+{
+    void Dataout(char* buffer); 
+}
+
+#endif
