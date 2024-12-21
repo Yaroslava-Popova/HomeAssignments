e30f2d1996f7310ccad3788a7a405a954b7e9110
Yaroslava <st132757@student.spbu.ru>
2024-12-21 07:18:35 +0300

    create the  header file

---
+++ Assignment2b/operation.h
@@ -0,0 +1,15 @@
+/* Popova Yaroslava
+st132757@student.spbu.ru
+*/
+
+#ifndef OPERATION_H
+#define OPERATION_H
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+bool operand(std::string oper);
+void func(float *a, float *b, int *index_mass, std::string *z);
+
+#endif
