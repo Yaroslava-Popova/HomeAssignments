09f3897225b6bfca2a7e54c3d9df392d2f17c0fb
Yaroslava <st132757@student.spbu.ru>
2025-04-01 15:16:34 +0300

    loading files

---
+++ Assignment3/Direction.cpp
@@ -0,0 +1,14 @@
+#include "Direction.h"
+
+Direction::Direction(std::string direction_name)
+    : _direction_name(direction_name) {}
+
+Direction::~Direction() {}
+
+std::string Direction::get_direction_name() {
+    return _direction_name;
+}
+
+void Direction::set_direction_name(std::string direction_name) {
+    _direction_name = direction_name;
+}
---
+++ Assignment3/Direction.h
@@ -0,0 +1,18 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+#include <string>
+
+class Direction {
+public:
+    Direction(std::string direction_name);
+    ~Direction();
+
+    std::string get_direction_name();
+    void set_direction_name(std::string direction_name);
+
+private:
+    std::string _direction_name;
+};
+
+#endif
